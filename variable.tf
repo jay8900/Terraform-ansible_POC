43b0e2f12783a5d264ae609de962d8a079186b35
jay8900 <90141704+jay8900@users.noreply.github.com>
2024-01-23 23:42:28 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "aws_ami_id" {
+  default = "ami-0b56b87b2fb2c3954"
+
+}
+
+variable "ssh_key_pair" {
+  default = "~/.ssh/id_rsa"
+}
+
+variable "ssh_key_pair_pub" {
+  default = "~/.ssh/id_rsa.pub"
+}
